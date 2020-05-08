68c2733aff564e2be19565d52013c14adc39e032
Wriju Ghosh <wghosh@microsoft.com>
2020-05-08 14:47:05 +0530

    second

---
+++ CSharp/FileListCS/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileListCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please enter the path:");
+           
+            var _folderPath = @"D:\OneDrive - Microsoft\Wriju_Personal\E_books_OneDriveBus\Bengali";
+            GetAllSubDirectories(_folderPath);
+
+        }
+
+        int iCount = 1;
+        static string GetAllSubDirectories(string folderPath, 
+                string fileSearchPattern = "*.*", bool isRecurse = true)
+        {
+            string allData = string.Empty;
+
+            EnumerationOptions recurse = new EnumerationOptions();
+            recurse.RecurseSubdirectories = true;
+            var directories = Directory.GetDirectories(folderPath, "*", recurse);
+            //var directories = Directory.GetDirectories(folderPath);
+            //Files in Root
+            allData = allData + GetFiles(folderPath);
+            //Files in Sub directories 
+            for (int i=0; i < directories.Length; i++)
+            {
+                allData = allData + GetFiles(directories[i]);                
+            }
+
+            return allData;
+        }
+
+        static string GetFiles(string directoryPath, string fileSearchPattern = "*.*"){
+            //Take the last part of the path as Folder Name to be displayed
+            string folderName = directoryPath.Substring(directoryPath.LastIndexOf("\\")+1);
+            string data = "\n" 
+                            + folderName + "\n" 
+                            + GenerateDash(folderName.Length) + "\n";
+
+            var files = Directory.GetFiles(directoryPath,"*.pdf");
+            for(int i=0; i< files.Length; i++){
+                data = data + "\n" + i.ToString("D2") + ". " + Path.GetFileName(files[i]);                
+            }
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine(data);
+            return data;
+        }
+
+        
+        static string GenerateDash(int len)
+        {
+            string retVal = "=";
+            for(int i = 0; i<len; i++){
+                retVal = retVal + "="; 
+            }   
+
+            return retVal;     
+        }
+    }
+
+
+}
